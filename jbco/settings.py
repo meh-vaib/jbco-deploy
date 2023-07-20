c7eadc090eda4286e92e1db3ed0b60f35f1469a5
Vaibhav Mehrotra <mehrotravaibhav09@gmail.com>
2023-07-20 17:47:38 +0530

    vercel deployment

--- jbco/settings.py
+++ jbco/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hrrre_6o+5$gcoq5*hp%4ze6uy%2h82q_4%+5*mccnux9jljd+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -122,6 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
 
 STATICFILES_DIRS = [
     BASE_DIR , "static",  
